c1a9a4f6198ab6edff78320f979dcb304fc48016
sava-stanchev <s.stanchev94@gmail.com>
2026-02-08 14:23:35 +0200

    initialize core-concurrency module with BookingSimulation and pom.xml

---
+++ core-concurrency/src/main/java/com/sava/booking/BookingSimulation.java
@@ -0,0 +1,8 @@
+package com.sava.booking;
+
+public class BookingSimulation {
+    public static void main(String[] args) {
+        System.out.println("Booking simulation starting...");
+    }
+}
+
